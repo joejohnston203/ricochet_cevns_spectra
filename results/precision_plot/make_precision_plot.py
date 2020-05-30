1aadd784ba9b3fd3a5349ff6443fc22251d73f2c
Joseph Johnston <jpj13@mit.edu>
2020-05-29 20:03:11 -0400

    Update precision plots

--- results/precision_plot/make_precision_plot.py
+++ results/precision_plot/make_precision_plot.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+from scipy.interpolate import make_interp_spline, BSpline
 
 if __name__ == "__main__":
     try:
@@ -17,14 +18,20 @@ if __name__ == "__main__":
     x_var = data_var[:,0]*data_var[:,1]/365.
     # Precision in pct
     y_var = data_var[:,4]*100.
-    plt.semilogx(x_var, y_var, 'r-', label="5% CEvNS Uncertainty")
+    spl_var = make_interp_spline(np.log10(x_var), y_var, k=3)
+    x_var_new = np.logspace(np.log10(x_var[0]), np.log10(x_var[-1]))
+    y_var_new = spl_var(np.log10(x_var_new))
+    plt.semilogx(x_var_new, y_var_new, 'r-', label="5% CEvNS Uncertainty")
 
     data_fix = np.loadtxt("lowe_nu_commercial_allLowE_precision_fixed/ge_bkgd_medium_1.0e+01_thresh_1.0e-02/precision_ge.txt")
     # Exposure in kg*years
     x_fix = data_fix[:,0]*data_fix[:,1]/365.
     # Precision in pct
     y_fix = data_fix[:,4]*100.
-    plt.semilogx(x_fix, y_fix, 'b-.', label="No CEvNS Uncertainty")
+    spl_fix = make_interp_spline(np.log10(x_fix), y_fix, k=3)
+    x_fix_new = np.logspace(np.log10(x_fix[0]), np.log10(x_fix[-1]))
+    y_fix_new = spl_fix(np.log10(x_fix_new))
+    plt.semilogx(x_fix_new, y_fix_new, 'b-.', label="No CEvNS Uncertainty")
 
     plt.axhline(y=1., color='k', linestyle=":")
     plt.axhline(y=5., color='k', linestyle=":")
@@ -37,17 +44,37 @@ if __name__ == "__main__":
 
     plt.savefig('plots/lowenu_comm_precision.png')
 
-
     # DEBUG PLOT: Varying low e envelope
     fig = plt.figure()
     fig.patch.set_facecolor('white')
+
+    data_var = np.loadtxt("lowe_nu_commercial_allLowE_precision_varying/ge_bkgd_conservative_1.0e+02_thresh_1.0e-02/precision_ge.txt")
+    # Exposure in kg*years
+    x_var = data_var[:,0]*data_var[:,1]/365.
+    # Precision in pct
+    y_var = data_var[:,4]*100.
+    plt.semilogx(x_var, y_var, linestyle='-', label="B=100, Conservative")
+
+    data_var = np.loadtxt("lowe_nu_commercial_allLowE_precision_varying/ge_bkgd_medium_1.0e+02_thresh_1.0e-02/precision_ge.txt")
+    # Exposure in kg*years
+    x_var = data_var[:,0]*data_var[:,1]/365.
+    # Precision in pct
+    y_var = data_var[:,4]*100.
+    plt.semilogx(x_var, y_var, linestyle='-', label="B=100, Medium")
 
     data_var = np.loadtxt("lowe_nu_commercial_allLowE_precision_varying/ge_bkgd_optimistic_1.0e+02_thresh_1.0e-02/precision_ge.txt")
     # Exposure in kg*years
     x_var = data_var[:,0]*data_var[:,1]/365.
     # Precision in pct
     y_var = data_var[:,4]*100.
-    plt.semilogx(x_var, y_var, linestyle='--', label="B=100, Optimistic")
+    plt.semilogx(x_var, y_var, linestyle='-', label="B=100, Optimistic")
+
+    data_var = np.loadtxt("lowe_nu_commercial_allLowE_precision_varying/ge_bkgd_conservative_1.0e+01_thresh_1.0e-02/precision_ge.txt")
+    # Exposure in kg*years
+    x_var = data_var[:,0]*data_var[:,1]/365.
+    # Precision in pct
+    y_var = data_var[:,4]*100.
+    plt.semilogx(x_var, y_var, linestyle='--', label="B=10, Conservative")
 
     data_var = np.loadtxt("lowe_nu_commercial_allLowE_precision_varying/ge_bkgd_medium_1.0e+01_thresh_1.0e-02/precision_ge.txt")
     # Exposure in kg*years
@@ -63,6 +90,27 @@ if __name__ == "__main__":
     y_var = data_var[:,4]*100.
     plt.semilogx(x_var, y_var, linestyle='--', label="B=10, Optimistic")
 
+    data_var = np.loadtxt("lowe_nu_commercial_allLowE_precision_varying/ge_bkgd_conservative_1.0e+00_thresh_1.0e-02/precision_ge.txt")
+    # Exposure in kg*years
+    x_var = data_var[:,0]*data_var[:,1]/365.
+    # Precision in pct
+    y_var = data_var[:,4]*100.
+    plt.semilogx(x_var, y_var, linestyle='--', label="B=1, Conservative")
+
+    data_var = np.loadtxt("lowe_nu_commercial_allLowE_precision_varying/ge_bkgd_medium_1.0e+00_thresh_1.0e-02/precision_ge.txt")
+    # Exposure in kg*years
+    x_var = data_var[:,0]*data_var[:,1]/365.
+    # Precision in pct
+    y_var = data_var[:,4]*100.
+    plt.semilogx(x_var, y_var, linestyle='--', label="B=1, Medium")
+
+    data_var = np.loadtxt("lowe_nu_commercial_allLowE_precision_varying/ge_bkgd_optimistic_1.0e+00_thresh_1.0e-02/precision_ge.txt")
+    # Exposure in kg*years
+    x_var = data_var[:,0]*data_var[:,1]/365.
+    # Precision in pct
+    y_var = data_var[:,4]*100.
+    plt.semilogx(x_var, y_var, linestyle='--', label="B=1, Optimistic")
+
     plt.axhline(y=1., color='k', linestyle=":")
     plt.axhline(y=5., color='k', linestyle=":")
     
@@ -75,38 +123,72 @@ if __name__ == "__main__":
 
     plt.savefig('plots/lowenu_comm_precision_debug_5.png')
 
-
     # DEBUG PLOT: Fixed low e envelope
     fig = plt.figure()
     fig.patch.set_facecolor('white')
+
+    data_fix = np.loadtxt("lowe_nu_commercial_allLowE_precision_fixed/ge_bkgd_conservative_1.0e+02_thresh_1.0e-02/precision_ge.txt")
+    # Exposure in kg*years
+    x_fix = data_fix[:,0]*data_fix[:,1]/365.
+    # Precision in pct
+    y_fix = data_fix[:,4]*100.
+    plt.semilogx(x_fix, y_fix, linestyle='-', label="B=100, Conservative")
 
     data_fix = np.loadtxt("lowe_nu_commercial_allLowE_precision_fixed/ge_bkgd_medium_1.0e+02_thresh_1.0e-02/precision_ge.txt")
     # Exposure in kg*years
     x_fix = data_fix[:,0]*data_fix[:,1]/365.
     # Precision in pct
     y_fix = data_fix[:,4]*100.
-    plt.semilogx(x_fix, y_fix, linestyle='-.', label="B=100, Medium")
+    plt.semilogx(x_fix, y_fix, linestyle='-', label="B=100, Medium")
 
     data_fix = np.loadtxt("lowe_nu_commercial_allLowE_precision_fixed/ge_bkgd_optimistic_1.0e+02_thresh_1.0e-02/precision_ge.txt")
     # Exposure in kg*years
     x_fix = data_fix[:,0]*data_fix[:,1]/365.
     # Precision in pct
     y_fix = data_fix[:,4]*100.
-    plt.semilogx(x_fix, y_fix, linestyle='-.', label="B=100, Optimistic")
+    plt.semilogx(x_fix, y_fix, linestyle='-', label="B=100, Optimistic")
+
+    data_fix = np.loadtxt("lowe_nu_commercial_allLowE_precision_fixed/ge_bkgd_conservative_1.0e+01_thresh_1.0e-02/precision_ge.txt")
+    # Exposure in kg*years
+    x_fix = data_fix[:,0]*data_fix[:,1]/365.
+    # Precision in pct
+    y_fix = data_fix[:,4]*100.
+    plt.semilogx(x_fix, y_fix, linestyle='--', label="B=10, Conservative")
 
     data_fix = np.loadtxt("lowe_nu_commercial_allLowE_precision_fixed/ge_bkgd_medium_1.0e+01_thresh_1.0e-02/precision_ge.txt")
     # Exposure in kg*years
     x_fix = data_fix[:,0]*data_fix[:,1]/365.
     # Precision in pct
     y_fix = data_fix[:,4]*100.
-    plt.semilogx(x_fix, y_fix, linestyle='-.', label="B=10, Medium")
+    plt.semilogx(x_fix, y_fix, linestyle='--', label="B=10, Medium")
 
     data_fix = np.loadtxt("lowe_nu_commercial_allLowE_precision_fixed/ge_bkgd_optimistic_1.0e+01_thresh_1.0e-02/precision_ge.txt")
     # Exposure in kg*years
     x_fix = data_fix[:,0]*data_fix[:,1]/365.
     # Precision in pct
     y_fix = data_fix[:,4]*100.
-    plt.semilogx(x_fix, y_fix, linestyle='-.', label="B=10, Optimistic")
+    plt.semilogx(x_fix, y_fix, linestyle='--', label="B=10, Optimistic")
+
+    data_fix = np.loadtxt("lowe_nu_commercial_allLowE_precision_fixed/ge_bkgd_conservative_1.0e+00_thresh_1.0e-02/precision_ge.txt")
+    # Exposure in kg*years
+    x_fix = data_fix[:,0]*data_fix[:,1]/365.
+    # Precision in pct
+    y_fix = data_fix[:,4]*100.
+    plt.semilogx(x_fix, y_fix, linestyle=':', label="B=1, Conservative")
+
+    data_fix = np.loadtxt("lowe_nu_commercial_allLowE_precision_fixed/ge_bkgd_medium_1.0e+00_thresh_1.0e-02/precision_ge.txt")
+    # Exposure in kg*years
+    x_fix = data_fix[:,0]*data_fix[:,1]/365.
+    # Precision in pct
+    y_fix = data_fix[:,4]*100.
+    plt.semilogx(x_fix, y_fix, linestyle=':', label="B=1, Medium")
+
+    data_fix = np.loadtxt("lowe_nu_commercial_allLowE_precision_fixed/ge_bkgd_optimistic_1.0e+00_thresh_1.0e-02/precision_ge.txt")
+    # Exposure in kg*years
+    x_fix = data_fix[:,0]*data_fix[:,1]/365.
+    # Precision in pct
+    y_fix = data_fix[:,4]*100.
+    plt.semilogx(x_fix, y_fix, linestyle=':', label="B=1, Optimistic")
 
     plt.axhline(y=1., color='k', linestyle=":")
     plt.axhline(y=5., color='k', linestyle=":")
