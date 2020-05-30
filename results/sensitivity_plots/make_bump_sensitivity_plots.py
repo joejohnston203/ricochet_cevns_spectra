e5ac8a3969031268792d6fbcc20e623fcd62ac9f
Joseph Johnston <jpj13@mit.edu>
2020-05-29 20:02:42 -0400

    Update sensitivity plots

--- results/sensitivity_plots/make_bump_sensitivity_plots.py
+++ results/sensitivity_plots/make_bump_sensitivity_plots.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MaxNLocator
 import os
+from scipy.interpolate import make_interp_spline, BSpline
 
 if __name__ == "__main__":
     try:
@@ -9,56 +10,84 @@ if __name__ == "__main__":
     except OSError as e:
         pass
 
-    # Make plots for Commercial Reactor, U-238 n Capture
+    # Make plots for Commercial Reactor,5 MeV Bump
 
-    fig1, ((a00, a01), (a10, a11)) = plt.subplots(2, 2)
+    fig1, (a00, a01, a10) = plt.subplots(3, 1)
     fig1.patch.set_facecolor('white')
-    fig1.set_figwidth(9.6)
-    fig1.set_figheight(7.2)
-    plt.subplots_adjust(wspace=0.3)
+    fig1.set_figwidth(5.2)
+    fig1.set_figheight(10.6)
+    #plt.subplots_adjust(wspace=0.3)
     plt.subplots_adjust(hspace=0.4)
 
-    x_thr      = [1.e-4, 2.e-4, 4.e-4, 8.e-4, 1.e-3]
-    y_thr      = [1.7e5, 2.2e5, 2.6e5, 2.5e5, 2.0e5]
-    y_low_thr  = [3.3e3, 2.7e3, 2.2e3, 2.1e3, 6.4e3]
-    y_high_thr = [1.0e7, 2.4e6, 1.6e6, 3.7e6, 3.7e6]
-    a00.plot(1.e3*np.array(x_thr), y_thr)
-    a00.fill_between(1.e3*np.array(x_thr), y_low_thr, y_high_thr,
-                     facecolor='c', color='c', alpha=0.5)
-    a00.set(xlabel="Threshold (eV)", ylabel="Required Exposure (kg*years)")
+    x_thr      = [0.0001, 0.0002, 0.0004, 0.0008, 0.0016, 0.0032, 0.0063, 0.0126, 0.0250, 0.0500]
+    y_thr      = [4.07e5, 4.62e5, 5.09e5, 3.79e5, 8.80e5, 2.57e5, 3.80e5, 4.13e5, 1.75e5, 7.80e5]
+
+    spl_thr = make_interp_spline(np.log10(x_thr), np.log10(y_thr), k=1)
+    x_thr_new = np.logspace(np.log10(x_thr[0]), np.log10(x_thr[-1]))
+    y_thr_new = np.power(10., spl_thr(np.log10(x_thr_new)))
+
+    y_avg = 0
+    for y in y_thr:
+        y_avg += y
+    y_avg = y_avg/float(len(y_thr))
+
+    #a00.plot(1.e3*x_thr_new, y_thr_new, color="k", linestyle="-")
+    a00.plot(1.e3*x_thr_new, 0.*x_thr_new+y_avg, color="k", linestyle="-")
+    a00.plot(1.e3*np.array(x_thr), y_thr, ".", color="k")
+    #a00.plot(x_thr, y_thr, "r:")
+    #a00.fill_between(x_thr, y_low_thr, y_high_thr,
+    #                 facecolor='c', color='c', alpha=0.5)
+    a00.axvline(1, color='k', linestyle=":")
+    a00.axvline(10, color='k', linestyle=":")
+    a00.axvline(50, color='k', linestyle=":")
+    a00.set(xlabel="Threshold (eV)", ylabel="Required Exposure (kg*years)",
+            title="5 MeV Bump")
     a00.set_xscale('log')
     a00.set_yscale('log')
+    a00.set_ylim(5.e4, 5.e6)
 
-    x_a      = [28.1, 65.4, 72.6]
-    y_a      = [9.8e5, 2.7e5, 2.0e5]
-    y_low_a  = [1.9e3, 6.4e3, 4.0e3]
-    y_high_a = [2.2e6, 2.1e6, 2.3e6]
-    a01.plot(x_a, y_a)
-    a01.fill_between(x_a, y_low_a, y_high_a,
-                     facecolor='c', color='c', alpha=0.5)
+    x_a       = [28.1,  65.4,  72.6]
+    y_a_worst = [1.1e5, 2.7e5, 6.9e5]
+    #y_a_med   = [0.28,  0.14,  0.13]
+    y_a_best  = [1.0e6, 3.0e6, 2.8e6]
+
+    a01.plot(x_a, y_a_worst, linestyle="--", color="#984ea3", label="Worst Case")
+    a01.plot(x_a, y_a_worst, ".", color="#984ea3")
+    #a01.plot(x_a, y_a_med, linestyle=":", color="#ff7f00", label="Medium Case")
+    #a01.plot(x_a, y_a_med, ".", color="#ff7f00")
+    a01.plot(x_a, y_a_best, linestyle="-.", color="#a65628", label="Best Case")
+    a01.plot(x_a, y_a_best, ".", color="#a65628")
+    #a01.fill_between(x_a, y_low_a, y_high_a,
+    #                 facecolor='c', color='c', alpha=0.5)
     a01.set(xlabel="A", ylabel="Required Exposure (kg*years)")
+    a01.legend()
     a01.set_yscale('log')
 
-    x_b      = [1.,    10.,   100.]
-    y_b      = [1.0e5, 4.8e4, 3.2e5]
-    y_low_b  = [4.9e3, 2.7e1, 6.3e1]
-    y_high_b = [5.9e6, 1.3e7, 1.9e7]
-    a10.plot(x_b, y_b)
-    a10.fill_between(x_b, y_low_b, y_high_b,
-                     facecolor='c', color='c', alpha=0.5)
+    x_b       = [1.,     1.7,    2.8,    4.6,    7.7,    12.9,   21.5,   36.,    60.,    100.]
+    #y_b_worst = []
+    #y_b_med   = []
+    y_b_best  = [4.17e5, 5.57e4, 1.72e4, 5.71e4, 1.01e6, 5.07e5, 6.67e5, 8.04e5, 7.04e5, 4.90e5]
+
+    #spl_b_worst = make_interp_spline(np.log10(x_b), np.log10(y_b_worst), k=3)
+    #spl_b_med = make_interp_spline(np.log10(x_b), np.log10(y_b_med), k=3)
+    spl_b_best = make_interp_spline(np.log10(x_b), np.log10(y_b_best), k=3)
+
+    x_b_new = np.logspace(np.log10(x_b[0]), np.log10(x_b[-1]))
+    #y_b_worst_new = np.power(10., spl_b_worst(np.log10(x_b_new)))
+    #y_b_med_new = np.power(10., spl_b_med(np.log10(x_b_new)))
+    y_b_best_new = np.power(10., spl_b_best(np.log10(x_b_new)))
+
+    #a10.plot(x_b_new, y_b_worst_new, color="#e41a1c", linestyle="--", label="Worst Shape")
+    #a10.plot(x_b, y_b_worst, ".", color="#e41a1c")
+    #a10.plot(x_b_new, y_b_med_new, color="#377eb8", linestyle=":", label="Medium Shape")
+    #a10.plot(x_b, y_b_med, ".", color="#377eb8")
+    a10.plot(x_b_new, y_b_best_new, color="#4daf4a", linestyle="-.", label="Optimistic Shape")
+    a10.plot(x_b, y_b_best, ".", color="#4daf4a")
+    #a10.fill_between(x_b, y_low_b, y_high_b,
+    #                 facecolor='c', color='c', alpha=0.5)
     a10.set(xlabel="Background Level (evts/kg/day)", ylabel="Required Exposure (kg*years)")
     a10.set_xscale('log')
     a10.set_yscale('log')
-
-    x_sh      = [1,    2,    3,  ]
-    y_sh      = [2.7e7, 2.5e7, 1.0e5]
-    y_low_sh  = [1.1e4, 2.9e2, 4.8e3]
-    y_high_sh = [1.0e10, 2.6e9, 5.9e6]
-    a11.plot(x_sh, y_sh)
-    a11.fill_between(x_sh, y_low_sh, y_high_sh,
-                     facecolor='c', color='c', alpha=0.5)
-    a11.set(xlabel="Background Shape", ylabel="Required Exposure (kg*years)")
-    a11.xaxis.set_major_locator(MaxNLocator(integer=True))
-    a11.set_yscale('log')
+    a10.legend()
 
     plt.savefig('plots/bump_sensitivity_plots.png')
--- results/sensitivity_plots/make_lowe_nu_sensitivity_plots.py
+++ results/sensitivity_plots/make_lowe_nu_sensitivity_plots.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MaxNLocator
 import os
+from scipy.interpolate import make_interp_spline, BSpline
 
 if __name__ == "__main__":
     try:
@@ -9,56 +10,76 @@ if __name__ == "__main__":
     except OSError as e:
         pass
 
-    # Make plots for Commercial Reactor, U-238 n Capture
+    # Make plots for Commercial Reactor, All Enu<1.8 MeV
 
-    fig1, ((a00, a01), (a10, a11)) = plt.subplots(2, 2)
+    fig1, (a00, a01, a10) = plt.subplots(3, 1)
     fig1.patch.set_facecolor('white')
-    fig1.set_figwidth(9.6)
-    fig1.set_figheight(7.2)
-    plt.subplots_adjust(wspace=0.3)
+    fig1.set_figwidth(5.2)
+    fig1.set_figheight(10.6)
+    #plt.subplots_adjust(wspace=0.3)
     plt.subplots_adjust(hspace=0.4)
 
-    x_thr      = [1.,   10.,  50.]
-    y_thr      = [5.6,  61.8, 1.0e9]
-    y_low_thr  = [3.7,  25.0, 1.1e7]
-    y_high_thr = [18.2, 62.0, 1.0e14]
-    a00.plot(x_thr, y_thr)
-    a00.fill_between(x_thr, y_low_thr, y_high_thr,
-                     facecolor='c', color='c', alpha=0.5)
+    x_thr      = [0.0001, 0.0002, 0.0004, 0.0008, 0.0016, 0.0032, 0.0063, 0.0126, 0.0250, 0.0500]
+    y_thr      = [0.0324, 0.0328, 0.0337, 0.0370, 0.0414, 0.0523, 0.0771, 0.152,  0.656,  16.5]
+
+    spl_thr = make_interp_spline(np.log10(x_thr), np.log10(y_thr), k=3)
+    x_thr_new = np.logspace(np.log10(x_thr[0]), np.log10(x_thr[-1]))
+    y_thr_new = np.power(10., spl_thr(np.log10(x_thr_new)))
+
+    a00.plot(1.e3*x_thr_new, y_thr_new, color="k", linestyle="-")
+    a00.plot(1.e3*np.array(x_thr), y_thr, ".", color="k")
+    #a00.plot(x_thr, y_thr, "r:")
+    #a00.fill_between(x_thr, y_low_thr, y_high_thr,
+    #                 facecolor='c', color='c', alpha=0.5)
+    a00.axvline(1, color='k', linestyle=":")
+    a00.axvline(10, color='k', linestyle=":")
+    a00.axvline(50, color='k', linestyle=":")
     a00.set(xlabel="Threshold (eV)", ylabel="Required Exposure (kg*years)")
     a00.set_xscale('log')
     a00.set_yscale('log')
 
-    x_a      = [28.1, 65.4, 72.6]
-    y_a      = [30.1, 27.4, 56.6]
-    y_low_a  = [12.8, 11.9, 25.0]
-    y_high_a = [53.2, 50.3, 61.8]
-    a01.plot(x_a, y_a)
-    a01.fill_between(x_a, y_low_a, y_high_a,
-                     facecolor='c', color='c', alpha=0.5)
-    a01.set(xlabel="A", ylabel="Required Exposure (kg*years)")
-    #a01.set_yscale('log')
+    x_a       = [28.1,  65.4,  72.6]
+    y_a_worst = [14.,   14.,   25.]
+    y_a_med   = [0.28,  0.14,  0.13]
+    y_a_best  = [0.081, 0.038, 0.034]
 
-    x_b      = [1.,   10.,  100.]
-    y_b      = [47.9, 61.8, 122.]
-    y_low_b  = [19.3, 25.0, 55.7]
-    y_high_b = [78.5, 62.0, 328.]
-    a10.plot(x_b, y_b)
-    a10.fill_between(x_b, y_low_b, y_high_b,
-                     facecolor='c', color='c', alpha=0.5)
+    a01.plot(x_a, y_a_worst, linestyle="--", color="#984ea3", label="Worst Case")
+    a01.plot(x_a, y_a_worst, ".", color="#984ea3")
+    a01.plot(x_a, y_a_med, linestyle=":", color="#ff7f00", label="Medium Case")
+    a01.plot(x_a, y_a_med, ".", color="#ff7f00")
+    a01.plot(x_a, y_a_best, linestyle="-.", color="#a65628", label="Best Case")
+    a01.plot(x_a, y_a_best, ".", color="#a65628")
+    #a01.fill_between(x_a, y_low_a, y_high_a,
+    #                 facecolor='c', color='c', alpha=0.5)
+    a01.set(xlabel="A", ylabel="Required Exposure (kg*years)")
+    a01.legend()
+    a01.set_yscale('log')
+
+    x_b       = [1.,    1.7,   2.8,   4.6,   7.7,   12.9,  21.5,  36.,   60.,   100.,  250.,  500., 1000.]
+    y_b_worst = [0.095, 0.103, 0.104, 0.107, 0.114, 0.149, 0.186, 0.275, 0.353, 0.471, 1.10,  1.95, 3.52]
+    y_b_med   = [0.095, 0.101, 0.103, 0.107, 0.112, 0.139, 0.157, 0.203, 0.248, 0.309, 0.601, 1.00, 2.10]
+    y_b_best  = [0.095, 0.100, 0.102, 0.105, 0.108, 0.132, 0.143, 0.165, 0.171, 0.179, 0.273, 0.57, 1.20]
+
+    spl_b_worst = make_interp_spline(np.log10(x_b), np.log10(y_b_worst), k=3)
+    spl_b_med = make_interp_spline(np.log10(x_b), np.log10(y_b_med), k=3)
+    spl_b_best = make_interp_spline(np.log10(x_b), np.log10(y_b_best), k=3)
+
+    x_b_new = np.logspace(np.log10(x_b[0]), np.log10(x_b[-1]))
+    y_b_worst_new = np.power(10., spl_b_worst(np.log10(x_b_new)))
+    y_b_med_new = np.power(10., spl_b_med(np.log10(x_b_new)))
+    y_b_best_new = np.power(10., spl_b_best(np.log10(x_b_new)))
+
+    a10.plot(x_b_new, y_b_worst_new, color="#e41a1c", linestyle="--", label="Worst Shape")
+    a10.plot(x_b, y_b_worst, ".", color="#e41a1c")
+    a10.plot(x_b_new, y_b_med_new, color="#377eb8", linestyle=":", label="Medium Shape")
+    a10.plot(x_b, y_b_med, ".", color="#377eb8")
+    a10.plot(x_b_new, y_b_best_new, color="#4daf4a", linestyle="-.", label="Optimistic Shape")
+    a10.plot(x_b, y_b_best, ".", color="#4daf4a")
+    #a10.fill_between(x_b, y_low_b, y_high_b,
+    #                 facecolor='c', color='c', alpha=0.5)
     a10.set(xlabel="Background Level (evts/kg/day)", ylabel="Required Exposure (kg*years)")
     a10.set_xscale('log')
-    #a10.set_yscale('log')
-
-    x_sh      = [1,    2,    3,  ]
-    y_sh      = [50.0, 55.0, 50.0]
-    y_low_sh  = [22.6, 25.0, 21.8]
-    y_high_sh = [55.5, 61.8, 54.4]
-    a11.plot(x_sh, y_sh)
-    a11.fill_between(x_sh, y_low_sh, y_high_sh,
-                     facecolor='c', color='c', alpha=0.5)
-    a11.set(xlabel="Background Shape", ylabel="Required Exposure (kg*years)")
-    a11.xaxis.set_major_locator(MaxNLocator(integer=True))
-    #11.set_yscale('log')
+    a10.set_yscale('log')
+    a10.legend()
 
     plt.savefig('plots/lowe_nu_sensitivity_plots.png')
