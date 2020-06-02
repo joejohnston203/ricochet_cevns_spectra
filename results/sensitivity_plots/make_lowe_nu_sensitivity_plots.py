ce9115df4f8e39b858a4bebfc71e5f63e98c4de0
Joseph Johnston <jpj13@mit.edu>
2020-06-02 10:09:06 -0400

    Combined threshold and A plots for lowe nu

--- results/sensitivity_plots/make_lowe_nu_sensitivity_plots.py
+++ results/sensitivity_plots/make_lowe_nu_sensitivity_plots.py
@@ -12,25 +12,41 @@ if __name__ == "__main__":
 
     # Make plots for Commercial Reactor, All Enu<1.8 MeV
 
-    fig1, (a00, a01, a10) = plt.subplots(3, 1)
+    fig1, (a00, a10) = plt.subplots(2, 1)
     fig1.patch.set_facecolor('white')
     fig1.set_figwidth(5.2)
-    fig1.set_figheight(10.6)
+    fig1.set_figheight(7.6)
     #plt.subplots_adjust(wspace=0.3)
     plt.subplots_adjust(hspace=0.4)
 
+    # Si
     x_thr      = [0.0001, 0.0002, 0.0004, 0.0008, 0.0016, 0.0032, 0.0063, 0.0126, 0.0250, 0.0500]
-    y_thr      = [0.0324, 0.0328, 0.0337, 0.0370, 0.0414, 0.0523, 0.0771, 0.152,  0.656,  16.5]
-
+    y_thr      = [0.141,  0.141,  0.144,  0.151,  0.160,  0.177,  0.208,  0.287,  0.567,  0.946]
     spl_thr = make_interp_spline(np.log10(x_thr), np.log10(y_thr), k=3)
     x_thr_new = np.logspace(np.log10(x_thr[0]), np.log10(x_thr[-1]))
     y_thr_new = np.power(10., spl_thr(np.log10(x_thr_new)))
+    a00.plot(1.e3*x_thr_new, y_thr_new, color="#984ea3", linestyle="--", label="Si (A=28.1)")
+    a00.plot(1.e3*np.array(x_thr), y_thr, ".", color="#984ea3")
 
-    a00.plot(1.e3*x_thr_new, y_thr_new, color="k", linestyle="-")
-    a00.plot(1.e3*np.array(x_thr), y_thr, ".", color="k")
-    #a00.plot(x_thr, y_thr, "r:")
-    #a00.fill_between(x_thr, y_low_thr, y_high_thr,
-    #                 facecolor='c', color='c', alpha=0.5)
+    # Zn
+    x_thr      = [0.0001, 0.0002, 0.0004, 0.0008, 0.0016, 0.0032, 0.0063, 0.0126, 0.0250, 0.0500]
+    y_thr      = [0.0380, 0.0387, 0.0395, 0.0436, 0.0477, 0.0599, 0.0876, 0.151,  0.619,  6.86]
+    spl_thr = make_interp_spline(np.log10(x_thr), np.log10(y_thr), k=3)
+    x_thr_new = np.logspace(np.log10(x_thr[0]), np.log10(x_thr[-1]))
+    y_thr_new = np.power(10., spl_thr(np.log10(x_thr_new)))
+    a00.plot(1.e3*x_thr_new, y_thr_new, color="#ff7f00", linestyle=":", label="Zn (A=65.4))")
+    a00.plot(1.e3*np.array(x_thr), y_thr, ".", color="#ff7f00")
+
+    # Ge
+    x_thr      = [0.0001, 0.0002, 0.0004, 0.0008, 0.0016, 0.0032, 0.0063, 0.0126, 0.0250, 0.0500]
+    y_thr      = [0.0324, 0.0328, 0.0337, 0.0370, 0.0414, 0.0523, 0.0771, 0.152,  0.656,  16.5]
+    spl_thr = make_interp_spline(np.log10(x_thr), np.log10(y_thr), k=3)
+    x_thr_new = np.logspace(np.log10(x_thr[0]), np.log10(x_thr[-1]))
+    y_thr_new = np.power(10., spl_thr(np.log10(x_thr_new)))
+    a00.plot(1.e3*x_thr_new, y_thr_new, color="#a65628", linestyle="-.", label="Ge (A=72.6)")
+    a00.plot(1.e3*np.array(x_thr), y_thr, ".", color="#a65628")
+
+    a00.legend()
     a00.axvline(1, color='k', linestyle=":")
     a00.axvline(10, color='k', linestyle=":")
     a00.axvline(50, color='k', linestyle=":")
@@ -43,7 +59,7 @@ if __name__ == "__main__":
     y_a_med   = [0.28,  0.14,  0.13]
     y_a_best  = [0.081, 0.038, 0.034]
 
-    a01.plot(x_a, y_a_worst, linestyle="--", color="#984ea3", label="Worst Case")
+    '''a01.plot(x_a, y_a_worst, linestyle="--", color="#984ea3", label="Worst Case")
     a01.plot(x_a, y_a_worst, ".", color="#984ea3")
     a01.plot(x_a, y_a_med, linestyle=":", color="#ff7f00", label="Medium Case")
     a01.plot(x_a, y_a_med, ".", color="#ff7f00")
@@ -53,7 +69,7 @@ if __name__ == "__main__":
     #                 facecolor='c', color='c', alpha=0.5)
     a01.set(xlabel="A", ylabel="Required Exposure (kg*years)")
     a01.legend()
-    a01.set_yscale('log')
+    a01.set_yscale('log')'''
 
     x_b       = [1.,    1.7,   2.8,   4.6,   7.7,   12.9,  21.5,  36.,   60.,   100.,  250.,  500., 1000.]
     y_b_worst = [0.095, 0.103, 0.104, 0.107, 0.114, 0.149, 0.186, 0.275, 0.353, 0.471, 1.10,  1.95, 3.52]
